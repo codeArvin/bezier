3aa40ed728947815418c8af60ca7af5e39fd1ec2
邵寒柳 <codearvin@gmail.com>
2019-02-02 16:08:12 +0800

    Updates

---
+++ precache-manifest.066eaa4999e838523008e65a486504e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "58b690e5d946c026192c",
+    "url": "/bezier/static/js/runtime~main.58b690e5.js"
+  },
+  {
+    "revision": "393a2373bd42e386a97a",
+    "url": "/bezier/static/js/main.393a2373.chunk.js"
+  },
+  {
+    "revision": "20d2636a2971ac09a549",
+    "url": "/bezier/static/js/1.20d2636a.chunk.js"
+  },
+  {
+    "revision": "393a2373bd42e386a97a",
+    "url": "/bezier/static/css/main.8e240f96.chunk.css"
+  },
+  {
+    "revision": "20d2636a2971ac09a549",
+    "url": "/bezier/static/css/1.f6182c87.chunk.css"
+  },
+  {
+    "revision": "4113c2227693ff3deda8c9b4b450d509",
+    "url": "/bezier/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ca7cdadd7a1e6935c8c919929d57f8e0.js"
+  "/bezier/precache-manifest.066eaa4999e838523008e65a486504e1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/bezier/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
